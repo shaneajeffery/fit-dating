9921337393919618290bd58ecab4609c206e7e3b
Shane Jeffery <shane.jeffery@mckesson.com>
2021-10-14 15:46:15 -0700

    Got sequelize setup with pg

--- api/index.js
+++ api/index.js
@@ -2,6 +2,7 @@ const { ApolloServer } = require('apollo-server');
 const { typeDefs } = require('./schema');
 const { resolvers } = require('./resolvers');
 
+// eslint-disable-next-line no-undef
 const port = process.env.PORT || 9090;
 
 const server = new ApolloServer({ resolvers, typeDefs });
---
+++ api/models/user.js
@@ -0,0 +1,20 @@
+'use strict';
+module.exports = (sequelize, DataTypes) => {
+  const User = sequelize.define(
+    'user',
+    {
+      username: {
+        type: DataTypes.STRING,
+      },
+      email: {
+        type: DataTypes.STRING,
+      },
+      password: {
+        type: DataTypes.STRING,
+      },
+    },
+    {}
+  );
+
+  return User;
+};
