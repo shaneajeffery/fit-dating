1a59fb27efd55c347a511deb267269c34aa9317e
Shane Jeffery <shane.jeffery@mckesson.com>
2021-10-20 17:20:21 -0700

    Finish up Activity CRUD

--- api/graphql/activity/resolvers.js
+++ api/graphql/activity/resolvers.js
@@ -1,6 +1,8 @@
+const { v4: uuidv4 } = require('uuid');
+
 module.exports = {
   Query: {
-    async getActivity(root, { id }, { models }) {
+    async getActivity(_, { id }, { models }) {
       return models.Activity.findByPk(id);
     },
     async listActivities(root, _, { models }) {
@@ -8,7 +10,45 @@ module.exports = {
     },
   },
 
-  // Will need to create mutations for create, update, delete.
+  Mutation: {
+    async createActivity(_, { name }, { models }) {
+      try {
+        return await models.Activity.create({
+          id: await uuidv4(),
+          name,
+        });
+      } catch (error) {
+        throw new Error(error.message);
+      }
+    },
 
-  Mutation: {},
+    async updateActivity(_, { id, name }, { models }) {
+      try {
+        const activity = await models.Activity.findByPk(id);
+
+        if (!activity) {
+          throw new Error(`Couldn’t find activity with id ${id}`);
+        }
+
+        activity.name = name;
+        await activity.save();
+
+        return activity;
+      } catch (error) {
+        throw new Error(error.message);
+      }
+    },
+
+    async deleteActivity(_, { id }, { models }) {
+      try {
+        const rowsDeleted = await models.Activity.destroy({ where: { id } });
+
+        return {
+          rowsDeleted,
+        };
+      } catch (error) {
+        throw new Error(error.message);
+      }
+    },
+  },
 };
--- api/graphql/activity/schema.js
+++ api/graphql/activity/schema.js
@@ -4,6 +4,16 @@ const typeDef = `
     getActivity(id: ID!): Activity
   }
 
+  extend type Mutation {
+    createActivity(name: String!): Activity!
+    updateActivity(id: String!, name: String!): Activity!
+    deleteActivity(id: String!): DeleteActivity!
+  }
+
+  type DeleteActivity {
+    rowsDeleted: Int!
+  }
+
   type Activity {
     id: String!
     name: String!
