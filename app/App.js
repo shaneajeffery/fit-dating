599c010c06b6fe8c2cff7c9871ec42390ea5ba8c
Shane Jeffery <shane.jeffery@mckesson.com>
2021-10-24 22:55:39 -0700

    Getting base routing working

--- app/App.js
+++ app/App.js
@@ -1,13 +1,48 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { persistCache } from 'apollo3-cache-persist';
+
+import HomeScreen from './src/HomeScreen';
+import SettingsScreen from './src/SettingsScreen';
+
+const cache = new InMemoryCache();
+
+const client = new ApolloClient({
+  uri: 'localhost:9090',
+  cache,
+  defaultOptions: { watchQuery: { fetchPolicy: 'cache-and-network' } },
+});
+
+const Tab = createBottomTabNavigator();
 
 export default function App() {
+  const [loadingCache, setLoadingCache] = useState(true);
+
+  useEffect(() => {
+    persistCache({
+      cache,
+      storage: AsyncStorage,
+    }).then(() => setLoadingCache(false));
+  }, []);
+
+  if (loadingCache) {
+    return <Text>Loading...</Text>;
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <ApolloProvider client={client}>
+      <NavigationContainer>
+        <Tab.Navigator>
+          <Tab.Screen name="Home" component={HomeScreen} />
+          <Tab.Screen name="Settings" component={SettingsScreen} />
+        </Tab.Navigator>
+      </NavigationContainer>
+    </ApolloProvider>
   );
 }
 
